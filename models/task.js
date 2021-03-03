47bfda1877554ef9d31638b25a6ff5ca3fabc49b
Omar Dario <melendrezomar2@gmail.com>
2021-03-03 20:58:04 -0300

    empezando proyecto de todoApp con js en consola

---
+++ models/task.js
@@ -0,0 +1,18 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class Task {
+    id = "";
+    title= "";
+    done = null;
+    created = null;
+    finished = null;
+
+    constructor(title) {
+        this.id = uuidv4();//TODO: asignar id
+        this.title = title;
+        this.done = false;
+        this.created = new Date();
+    }
+}
+
+module.exports = Task;
